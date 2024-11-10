a10c108508e3f74a57593eb5c7692445506b344b
Sivakranth-bfw <venkatsivakranth@gmail.com>
2024-11-10 18:13:53 +0530

    Styles added

--- src/Core/Header/Header.js
+++ src/Core/Header/Header.js
@@ -7,24 +7,24 @@ export const Header = () => {
   return (
     <>
       {[ 'md'].map((expand) => (
-        <Navbar key={expand} expand={expand} className="bg-body-tertiary mb-3">
+        <Navbar key={expand} expand={expand} className="navbar">
           <Container fluid>
             <Navbar.Brand href="#"><img src={logo}></img></Navbar.Brand>
             <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
             <Navbar.Offcanvas
               id={`offcanvasNavbar-expand-${expand}`}
               aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
-              placement="end"
+              placement="start"
             >
               <Offcanvas.Header closeButton>
-                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
-                  Offcanvas
-                </Offcanvas.Title>
               </Offcanvas.Header>
               <Offcanvas.Body>
                 <Nav className="justify-content-end flex-grow-1 pe-3">
-                  <Nav.Link href="#action1">Home</Nav.Link>
-                  <Nav.Link href="#action2">Link</Nav.Link>
+                  <Nav.Link href="#action1" className="nav-links active ">Home</Nav.Link>
+                  <Nav.Link href="#action2" className="nav-links">Our Menu</Nav.Link>
+                  <Nav.Link href="#action3" className="nav-links">Gallery</Nav.Link>
+                  <Nav.Link href="#action4" className="nav-links">About Us</Nav.Link>
+                  <Nav.Link href="#action5" className="nav-links">Contact</Nav.Link>
                 </Nav>
               </Offcanvas.Body>
             </Navbar.Offcanvas>
